37fd83cda47b628664351b7a64eb8e867a902813
edwardyq67 <edwardyq67@gmail.com>
2024-10-01 13:36:25 -0500

    casi listo1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
+import netlify from '@astrojs/netlify'; 
 /**
  * @type {import('astro/config').AstroUserConfig}
  */
@@ -11,5 +11,6 @@ export default defineConfig({
   // Integraciones
   integrations: [tailwind(), react()],
   // Salida del proyecto: estático o servidor
-  output: "server" // Cambiar a "static" si prefieres salida estática
+  output: "server", // Cambiar a "static" si prefieres salida estática
+  adapter: netlify(),
 });
