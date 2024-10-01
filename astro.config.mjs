8865a3606118968375134c1dc5354913d7aec1f5
edwardyq67 <edwardyq67@gmail.com>
2024-10-01 14:56:20 -0500

    casi listo5

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,23 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify'; 
+
 /**
  * @type {import('astro/config').AstroUserConfig}
  */
 export default defineConfig({
   // Integraciones
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), netlify()],
   // Salida del proyecto: estático o servidor
   output: "server", // Cambiar a "static" si prefieres salida estática
   adapter: netlify(),
+  
+  // Configuración de Vite
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['zustand'] // Asegúrate de que sea en minúsculas
+      }
+    }
+  }
 });
