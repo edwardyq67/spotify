3336827847e19fe50f0ab7301c1497841da98b77
edwardyq67 <edwardyq67@gmail.com>
2024-10-01 13:32:51 -0500

    casi listo4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node'; // Importa el adaptador de Node.js
 
 /**
  * @type {import('astro/config').AstroUserConfig}
@@ -12,6 +11,5 @@ export default defineConfig({
   // Integraciones
   integrations: [tailwind(), react()],
   // Salida del proyecto: estático o servidor
-  output: "server", // Modo servidor
-  adapter: node(),  // Configura el adaptador de Node.js para el modo servidor
+  output: "server" // Cambiar a "static" si prefieres salida estática
 });
