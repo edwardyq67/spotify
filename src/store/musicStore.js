60cac6dddeee47232a5a52a8de44007bb7fbdd3f
edwardyq67 <edwardyq67@gmail.com>
2024-10-01 13:56:16 -0500

    casi listo2

--- src/store/musicStore.js
+++ src/store/musicStore.js
@@ -2,7 +2,7 @@ import {create} from "Zustand";
 
 export const usePlaterStore=create((set)=>({
     isPlaying:false,
-    currentMusic:{playlists:null,song:null,song:[]},
+    currentMusic:{playlists:null,song:null,songs:[]},
     volume:1,
     setVolume:(volume)=>set({volume}),
     setIsPlaying:(isPlaying)=>set({isPlaying}),
